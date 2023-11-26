d723e0f7ffbc15a246614a78c9d83438948c3b78
Caitlin Starkey <bluemoon.lunacy@gmail.com>
2023-11-26 17:29:21 -0500

    had the wrong type of class libraries

---
+++ ReptileHusbandryData/HusbandryContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace ReptileHusbandryData
+{
+    public class HusbandryContext
+    {
+
+    }
+}
---
+++ ReptileHusbandryDomain/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace ReptileHusbandryDomain
+{
+    public class Class1
+    {
+
+    }
+}
